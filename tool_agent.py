acef433d6e8014100ccbf5e7bd5fc1f2a90c3330
Shivanker Goel <shivanker.goel@gmail.com>
2024-05-06 21:29:42 +0000

    Use ChatGenerator

--- tool_agent.py
+++ tool_agent.py
@@ -4,11 +4,12 @@ import json
 from typing import List, Optional
 
 from haystack import Pipeline, component  # type: ignore
-from haystack.components.builders import PromptBuilder  # type: ignore
+from haystack.dataclasses import ChatMessage  # type: ignore
+from haystack.components.builders import PromptBuilder, DynamicChatPromptBuilder  # type: ignore
 from pydantic import BaseModel, ValidationError
 from haystack.components.generators import OpenAIGenerator  # type: ignore
+from haystack.components.generators.chat import OpenAIChatGenerator  # type: ignore
 
-from chat_models import *
 
 # Configure the argument parser
 parser = argparse.ArgumentParser()
@@ -21,6 +22,7 @@ logging.basicConfig(
     format="%(levelname).1s%(asctime)s %(filename)s:%(lineno)d] %(message)s",
     datefmt="%m%d %H:%M:%S",
 )
+logger = logging.getLogger()
 
 
 class City(BaseModel):
@@ -42,38 +44,45 @@ class OutputValidator:
 
     # Define the component output
     @component.output_types(
-        valid_replies=List[str],
-        invalid_replies=Optional[List[str]],
-        error_message=Optional[str],
+        valid_replies=List[ChatMessage],
+        invalid_replies=List[str],
+        error_message=str,
     )
-    def run(self, replies: List[str]):
+    def run(self, replies: List[ChatMessage]):
 
         self.iteration_counter += 1
-        logging.getLogger().info("Got message from LLM: %s", replies[0])
+        logger.info("Got message from LLM: %s", replies[0])
 
         ## Try to parse the LLM's reply ##
         # If the LLM's reply is a valid object, return `"valid_replies"`
         try:
-            output_dict = json.loads(replies[0])
+            output_dict = json.loads(replies[0].content)
             self.pydantic_model.parse_obj(output_dict)
-            print(
+            (
                 f"OutputValidator at Iteration {self.iteration_counter}: Valid JSON from LLM - No need for looping: {replies[0]}"
             )
-            return {"valid_replies": replies}
+            return {
+                "valid_replies": replies,
+                "invalid_replies": [],
+                "error_message": "",
+            }
 
         # If the LLM's reply is corrupted or not valid, return "invalid_replies" and the "error_message" for LLM to try again
         except (ValueError, ValidationError) as e:
-            print(
+            logger.error(
                 f"OutputValidator at Iteration {self.iteration_counter}: Invalid JSON from LLM - Let's try again.\n"
                 f"Output from LLM:\n {replies[0]} \n"
                 f"Error from OutputValidator: {e}"
             )
-            return {"invalid_replies": replies, "error_message": str(e)}
+            return {"invalid_replies": [replies[0].content], "error_message": str(e)}
 
 
-prompt_template = """
+prompt_template = [
+    ChatMessage.from_user(
+        """
 Create a JSON object from the information present in this passage: {{passage}}.
-Only use information that is present in the passage. Follow this JSON schema, but only return the actual instances without any additional schema definition:
+Only use information that is present in the passage. 
+Follow this JSON schema, but only return the actual instances without any additional schema definition:
 {{schema}}
 Make sure your response is a dict and not a list.
 {% if invalid_replies and error_message %}
@@ -82,29 +91,27 @@ Make sure your response is a dict and not a list.
   Correct the output and try again. Just return the corrected output without any extra explanations.
 {% endif %}
 """
-prompt_builder = PromptBuilder(
-    template=prompt_template, required_variables=["passage", "schema"]
+    )
+]
+prompt_builder = DynamicChatPromptBuilder(
+    runtime_variables=["invalid_replies", "error_message"]
 )
 output_validator = OutputValidator(pydantic_model=CitiesData)  # type: ignore
 
 
-llama8 = OpenAIGenerator(
-    model="llama3-8b-8192",
-    api_base_url="https://api.groq.com/openai/v1",
-    api_key=Secret.from_env_var("GROQ_API_KEY"),
-)
+generator = OpenAIChatGenerator(model="gpt-4-turbo")
 
 
 pipeline = Pipeline(max_loops_allowed=5)
 
 # Add components to your pipeline
 pipeline.add_component(instance=prompt_builder, name="prompt_builder")
-pipeline.add_component(instance=llama8, name="llm")
+pipeline.add_component(instance=generator, name="llm")
 pipeline.add_component(instance=output_validator, name="output_validator")
 
 # Now, connect the components to each other
-pipeline.connect("prompt_builder", "llm")
-pipeline.connect("llm", "output_validator")
+pipeline.connect("prompt_builder.prompt", "llm.messages")
+pipeline.connect("llm", "output_validator.replies")
 # If a component has more than one output or input, explicitly specify the connections:
 pipeline.connect("output_validator.invalid_replies", "prompt_builder.invalid_replies")
 pipeline.connect("output_validator.error_message", "prompt_builder.error_message")
@@ -112,3 +119,12 @@ pipeline.draw("./auto-correct-pipeline.png")  # type: ignore
 
 json_schema = CitiesData.schema_json(indent=2)
 passage = "Berlin is the capital of Germany. It has a population of 3.850.809. Paris, France's capital, has 2.161 million residents. Lisbon is the capital and the largest city of Portugal with the population of 504,718."
+
+res = pipeline.run(
+    data={
+        "prompt_builder": {
+            "template_variables": {"passage": passage, "schema": json_schema},
+            "prompt_source": prompt_template,
+        },
+    }
+)
