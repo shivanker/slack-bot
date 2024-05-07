bc5c6f518b1d2fe6ccd9810f503c7a9012190f7c
Shivanker Goel <shivanker.goel@gmail.com>
2024-05-07 21:57:12 +0000

    Work around DynamicChatPromptBuilder bug

--- tool_agent.py
+++ tool_agent.py
@@ -1,14 +1,14 @@
 import argparse
-import logging
 import json
-from typing import List, Optional
+import logging
+from typing import Any, List, Optional
 
 from haystack import Pipeline, component  # type: ignore
-from haystack.dataclasses import ChatMessage  # type: ignore
-from haystack.components.builders import PromptBuilder, DynamicChatPromptBuilder  # type: ignore
-from pydantic import BaseModel, ValidationError
-from haystack.components.generators import OpenAIGenerator  # type: ignore
+from haystack.components.builders import DynamicChatPromptBuilder  # type: ignore
 from haystack.components.generators.chat import OpenAIChatGenerator  # type: ignore
+from haystack.dataclasses import ChatMessage  # type: ignore
+from chat_models import *
+from pydantic import BaseModel, ValidationError
 
 
 # Configure the argument parser
@@ -18,11 +18,11 @@ args = parser.parse_args()
 
 # Configure logger
 logging.basicConfig(
-    level=getattr(logging, args.log_level.upper()),
     format="%(levelname).1s%(asctime)s %(filename)s:%(lineno)d] %(message)s",
     datefmt="%m%d %H:%M:%S",
 )
-logger = logging.getLogger()
+logger = logging.getLogger(__name__)
+logger.setLevel(getattr(logging, args.log_level.upper()))
 
 
 class City(BaseModel):
@@ -45,43 +45,43 @@ class OutputValidator:
     # Define the component output
     @component.output_types(
         valid_replies=List[ChatMessage],
-        invalid_replies=List[str],
-        error_message=str,
+        invalid_replies=Optional[List[str]],
+        error_message=Optional[str],
     )
     def run(self, replies: List[ChatMessage]):
 
         self.iteration_counter += 1
-        logger.info("Got message from LLM: %s", replies[0])
+        logger.debug(f"[{self.iteration_counter}] Got message from LLM: \n{replies[0]}")
 
         ## Try to parse the LLM's reply ##
         # If the LLM's reply is a valid object, return `"valid_replies"`
         try:
-            output_dict = json.loads(replies[0].content)
+            content = replies[0].content
+            if content.count("```") >= 2:
+                output_dict = json.loads(content.split("```")[1])
+            else:
+                output_dict = json.loads(content)
             self.pydantic_model.parse_obj(output_dict)
-            (
+            logger.info(
                 f"OutputValidator at Iteration {self.iteration_counter}: Valid JSON from LLM - No need for looping: {replies[0]}"
             )
-            return {
-                "valid_replies": replies,
-                "invalid_replies": [],
-                "error_message": "",
-            }
+            return {"valid_replies": replies}
 
         # If the LLM's reply is corrupted or not valid, return "invalid_replies" and the "error_message" for LLM to try again
         except (ValueError, ValidationError) as e:
             logger.error(
                 f"OutputValidator at Iteration {self.iteration_counter}: Invalid JSON from LLM - Let's try again.\n"
-                f"Output from LLM:\n {replies[0]} \n"
+                f"Output from LLM:\n {replies[0].content} \n"
                 f"Error from OutputValidator: {e}"
             )
             return {"invalid_replies": [replies[0].content], "error_message": str(e)}
 
 
-prompt_template = [
+messages = [
     ChatMessage.from_user(
         """
 Create a JSON object from the information present in this passage: {{passage}}.
-Only use information that is present in the passage. 
+Only use information that is present in the passage.
 Follow this JSON schema, but only return the actual instances without any additional schema definition:
 {{schema}}
 Make sure your response is a dict and not a list.
@@ -96,25 +96,25 @@ Make sure your response is a dict and not a list.
 prompt_builder = DynamicChatPromptBuilder(
     runtime_variables=["invalid_replies", "error_message"]
 )
+for optional_var in ["invalid_replies", "error_message"]:
+    component.set_input_type(prompt_builder, optional_var, Optional[Any], None)
+
 output_validator = OutputValidator(pydantic_model=CitiesData)  # type: ignore
-
-
-generator = OpenAIChatGenerator(model="gpt-4-turbo")
 
 
 pipeline = Pipeline(max_loops_allowed=5)
 
 # Add components to your pipeline
 pipeline.add_component(instance=prompt_builder, name="prompt_builder")
-pipeline.add_component(instance=generator, name="llm")
-pipeline.add_component(instance=output_validator, name="output_validator")
-
-# Now, connect the components to each other
+pipeline.add_component(instance=LLAMA3_8B, name="llm")
 pipeline.connect("prompt_builder.prompt", "llm.messages")
+
+
+pipeline.add_component(instance=output_validator, name="output_validator")
 pipeline.connect("llm", "output_validator.replies")
-# If a component has more than one output or input, explicitly specify the connections:
 pipeline.connect("output_validator.invalid_replies", "prompt_builder.invalid_replies")
 pipeline.connect("output_validator.error_message", "prompt_builder.error_message")
+
 pipeline.draw("./auto-correct-pipeline.png")  # type: ignore
 
 json_schema = CitiesData.schema_json(indent=2)
@@ -124,7 +124,9 @@ res = pipeline.run(
     data={
         "prompt_builder": {
             "template_variables": {"passage": passage, "schema": json_schema},
-            "prompt_source": prompt_template,
+            "prompt_source": messages,
         },
     }
 )
+
+print("Structured output from LLM: \n" + res["output_validator"]["valid_replies"][0].content)  # type: ignore
