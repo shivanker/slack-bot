7b0d8f8781954e74c4a1a56a7b0f3d52926b52a5
Shivanker Goel <shivanker.goel@gmail.com>
2024-10-23 03:52:44 +0100

    Update Sonnet

--- lite_llms.py
+++ lite_llms.py
@@ -9,7 +9,7 @@ class TextModel(Enum):
     O1_PREVIEW = "o1-preview"
     O1_MINI = "o1-mini"
     CLAUDE_3_OPUS = "claude-3-opus-20240229"
-    CLAUDE_35_SONNET = "claude-3-5-sonnet-20240620"
+    CLAUDE_35_SONNET = "claude-3-5-sonnet-20241022"
     CLAUDE_3_HAIKU = "claude-3-haiku-20240307"
     GEMINI_15_PRO = "gemini/gemini-1.5-pro-latest"
     GEMINI_15_FLASH = "gemini/gemini-1.5-flash-latest"
